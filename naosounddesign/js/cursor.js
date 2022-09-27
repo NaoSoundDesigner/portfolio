146919c11ab1238a340ebc4ca3f975157c72d2fd
NaoSoundDesigner <114534389+NaoSoundDesigner@users.noreply.github.com>
2022-09-27 23:56:02 +0200

    Delete naosounddesign directory

--- naosounddesign/js/cursor.js
+++
@@ -1,6 +0,0 @@
-const cursor = document.querySelector(".cursor-inner");
-const cursor2 = document.querySelector(".cursor-outer");
-document.addEventListener("mousemove", e=>{
-    cursor.style.top = e.pageY
-    cursor.style.left = e.pageX
-})
